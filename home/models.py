5d84ed9a7262a660523446d70d3ad754b20f593d
Siji B J <siji.bj@experionglobal.com>
2020-06-11 17:53:51 +0530

    Done home page: Title and image

--- home/models.py
+++ home/models.py
@@ -1,7 +1,17 @@
 from django.db import models
-
+from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.core.models import Page
 
 
 class HomePage(Page):
-    pass
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Homepage image'
+    )
+    content_panels = Page.content_panels + [
+        ImageChooserPanel('image')
+    ]
