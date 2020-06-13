65ee4a09feb98fe4424443e0c07d0a7423d13cf6
Siji B J <siji.bj@experionglobal.com>
2020-06-13 10:43:56 +0530

    Created Objectives in HomePage Model

--- home/models.py
+++ home/models.py
@@ -14,36 +14,60 @@ class HomePage(Page):
     image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
-        blank=True,
+        blank=False,
         on_delete=models.SET_NULL,
         related_name='+',
         help_text='Homepage image'
     )
     vision_title = models.CharField(
         null=True,
-        blank=True,
+        blank=False,
+        default='Vision',
         max_length=100,
         help_text='Vision'
     )
     vision_image = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
-        blank=True,
+        blank=False,
         on_delete=models.SET_NULL,
         related_name='+',
         help_text='Vision image'
     )
     vision_text = RichTextField(
         null=True,
-        blank=True,
+        blank=False,
+        default=' ',
         max_length=400,
         help_text='Write vision'
     )
     mission_title = models.CharField(
-        null=True,
-        blank=True,
+        blank=False,
+        default='Mission',
         max_length=250,
         help_text='Mission title'
+    )
+    objectives_title = models.CharField(
+        blank=False,
+        default='Objectives',
+        max_length=250,
+        help_text='Objective title'
+        )
+    objectives_background_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Background Image Of Objectives Section'
+    )
+    objectives_side_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Image displayed in the right side of objectives section'
     )
     content_panels = Page.content_panels + [
         ImageChooserPanel(
@@ -56,7 +80,13 @@ class HomePage(Page):
         MultiFieldPanel([
             FieldPanel('mission_title',classname="full"),
             InlinePanel('missions',label="Mission"),
-        ], heading="Missions")
+        ], heading="Missions"),
+        MultiFieldPanel([
+            FieldPanel('objectives_title',classname="full"),
+            ImageChooserPanel('objectives_background_image',classname="full"),
+            ImageChooserPanel('objectives_side_image',classname="full"),
+            InlinePanel('objectives',label="Objective"),
+        ], heading="Objectives")
         
     ]
 class Mission(Orderable):
@@ -64,13 +94,36 @@ class Mission(Orderable):
     list_label = models.ForeignKey(
         'wagtailimages.Image',
         null=True,
-        blank=True,
+        blank=False,
         on_delete=models.SET_NULL,
         related_name='+',
         help_text='List label'
     )
-    mission = models.CharField(blank=True, max_length=250)
+    mission = models.CharField(
+        blank=False,
+        default=' ',
+        max_length=250
+    )
     panels= [
         ImageChooserPanel('list_label'),
         FieldPanel('mission')
+    ]
+class Objective(Orderable):
+    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='objectives')
+    objective_text = models.CharField(
+        blank=False,
+        default=' ',
+        max_length=250
+    )
+    objective_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Images of Objectives'
+    )
+    panels = [
+        FieldPanel('objective_text'),
+        ImageChooserPanel('objective_image')
     ]
