baeb4607d135a0906b4033e5470cbac7986df0b7
Siji B J <siji.bj@experionglobal.com>
2020-06-16 17:06:25 +0530

    added new app contactus and its model class

--- XPort/settings/base.py
+++ XPort/settings/base.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'home',
     'search',
     'consultansandpartners',
-    
+    'contactus',
+
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
